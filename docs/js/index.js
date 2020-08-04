270fc79a27b247fbc483a88ce1c7e23daaebf153
nagisa mashima <nagisa0823mashima@gmail.com>
2020-08-04 17:53:56 +0900

    カラー

--- docs/js/index.js
+++ docs/js/index.js
@@ -37,7 +37,7 @@ var scrollElm = (function() {
 $(window).scroll(function(){
  var windowHeight = $(window).height(),
      topWindow = $(window).scrollTop();
- $('#animation').each(function(){
+$('#animation').each(function(){
   var targetPosition = $(this).offset().top;
   if(topWindow > targetPosition - windowHeight + 100){
    $(this).addClass("fadeInDown");
