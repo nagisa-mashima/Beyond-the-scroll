13cfe323968050768c3b2e8e241b21c75f589a8e
nagisa mashima <nagisa0823mashima@gmail.com>
2020-08-04 22:24:36 +0900

    まんあ、アニメ調整

--- docs/js/index.js
+++ docs/js/index.js
@@ -27,7 +27,6 @@ var scrollElm = (function () {
   
   // セクション要素のdata-z属性を取得し、transformを設定
   // 最後のセクション要素のdata-zを元に、画面の高さを計算して設定
-  
   for (var i = 0; sections.length > i; i++) {
     var itemZ = sections[i].getAttribute("data-z");
     sections[i].style.transform = "translateZ(" + -itemZ + "px)";
@@ -39,16 +38,4 @@ var scrollElm = (function () {
   // スクロールイベントで、#scaler要素のtransformでz軸を動かす
   window.addEventListener("scroll", function () {
     scaler.style.transform = "translateZ(" + scrollElm.scrollTop / 100 + "px)";
-  });
-  
-  $("#animation").css("visibility", "hidden");
-  $(window).scroll(function () {
-    var windowHeight = $(window).height(),
-      topWindow = $(window).scrollTop();
-    $("#animation").each(function () {
-      var targetPosition = $(this).offset().top;
-      if (topWindow > targetPosition - windowHeight + 500) {
-        $(this).addClass("fadeInDown");
-      }
-    });
   });
